37d7cb166cc4cb239fe04aeb874ef86088173923
alex <AlexTheProgrammer@hotmail.com>
2018-08-18 23:20:30 -0700

    models for profiles and social profiles configured

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,2 +1,4 @@
 class Profile < ApplicationRecord
+  has_many :profiles_social_profiles
+  has_many :social_profiles, through: :profiles_solial_profiles
 end
--- app/models/social_profile.rb
+++ app/models/social_profile.rb
@@ -1,2 +1,6 @@
 class SocialProfile < ApplicationRecord
+  has_many :profiles_solial_profiles
+  has_many :profiles, through: :profiles_social_profiles
+
+  validates_presence_of :social_profile_type, :profile_url
 end
